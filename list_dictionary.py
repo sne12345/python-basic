111a53abe0fc41b8cee2617994ff11da4a4f7332
Naeun Song <51853700+sne12345@users.noreply.github.com>
2021-01-15 16:15:17 +0900

    Delete list_dictionary.py

--- list_dictionary.py
+++
@@ -1,8 +0,0 @@
-products = {"풀" : 1800, "색종이" : 1000}
-
-for product in products.items():
-    print("{}은 {}원이다.".format(product[0], product[1]))
-
-list = ['개', '고양이', '사자']
-for product in enumerate(list):
-    print("{}번째 값은 {}이다.".format(product[0], product[1]))
