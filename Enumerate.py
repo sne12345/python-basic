7e671ea715d13574e4dea493804943c361977462
Naeun Song <51853700+sne12345@users.noreply.github.com>
2021-01-15 16:13:55 +0900

    Delete Enumerate.py

--- Enumerate.py
+++
@@ -1,8 +0,0 @@
-for i,j in enumerate(['tic','tac','toe']):
-    print(i,j)
-
-myList = ['a','b','c','d']
-print(list(enumerate(myList)))
-
-myTuple = {i:j for i,j in enumerate('Apple Banana Computer Dictionary Eraser Food'.split())}
-print(myTuple)
